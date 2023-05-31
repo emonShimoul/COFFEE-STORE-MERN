42525efc298db2930318afdaef3a37b51a665eb7
emonShimoul <emonshimoul@gmail.com>
2023-06-01 00:30:15 +0600

    implement the delete operation

--- coffee-store-client/src/components/CoffeeCard.jsx
+++ coffee-store-client/src/components/CoffeeCard.jsx
@@ -1,5 +1,33 @@
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, photo } = coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your Coffee has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="card card-side bg-base-100 shadow-xl">
@@ -17,7 +45,12 @@ const CoffeeCard = ({ coffee }) => {
           <div className="btn-group btn-group-vertical space-y-4 mt-4">
             <button className="btn">View</button>
             <button className="btn">Edit</button>
-            <button className="btn">X</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-red-500"
+            >
+              X
+            </button>
           </div>
         </div>
       </div>
